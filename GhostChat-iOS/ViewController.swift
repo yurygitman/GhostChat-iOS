7b6895674d764252f166f1aada7994e52a049b1a
samuraipapa <yury.gitman@gmail.com>
2015-04-26 12:46:14 -0400

    Initial Commit

---
+++ GhostChat-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GhostChat-iOS
+//
+//  Created by GrownYoda on 4/26/15.
+//  Copyright (c) 2015 yuryg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
